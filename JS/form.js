cd1884902e7c8aa4c5afaa5a4fe5e102220b80c4
ivan-carballo <149379998+ivan-carballo@users.noreply.github.com>
2024-04-24 19:57:32 +0200

    Update form.js

--- JS/form.js
+++ JS/form.js
@@ -1,15 +1,4 @@
 // Aqui ira todo el codigo referente al formulario y el envio de mensajes
-/* import require from '/require';
-import { apiKey_mail, user } from './API.js';
-
-
-
-const ElasticEmail = require('@elasticemail/elasticemail-client');
-const client = ElasticEmail.ApiClient.instance;
-
-const apikey = client.authentications['apikey'];
-apikey.apiKey = apiKey_mail; */
-
 
 
 // Hacer getElementByID a todos los inputs
@@ -60,39 +49,3 @@ buttonEnviar.addEventListener('click', (e) => {
         rotuloEnviar.style.fontSize = '5rem';
     }
 });
-
-
-// Funcion del boton 'Enviar' que manda el formulario al correo
-/* const buttonEnviar = document.getElementById('buttonSend');
-buttonEnviar.addEventListener('click', (e) => {
-    const emailsApi = new ElasticEmail.EmailsApi();
-
-    const emailData = {
-    Recipients: {
-        To: [user]
-    },
-    Content: {
-        Body: [
-            {
-                ContentType: "HTML",
-                Charset: "utf-8",
-                Content: `Mensaje entrante:<br>${delInputTextarea.value}<br> Contestar a: ${delInputEmail}`
-            }
-        ],
-        From: user,
-        Subject: `Mensaje de ${delInputName.value} ${inputSurname.value} desde MichiPedia`
-    }
-    };
-
-    const callback = (error, data, response) => {
-    if (error) {
-        rotuloSend.innerHTML = 'El mensaje no se ha podido entregar';
-        console.error(error);
-    } else {
-        rotuloSend.innerHTML = 'El mensaje ha sido enviado correctamente';
-        console.log('API called successfully.');
-        console.log('Email sent.');
-    }
-    };
-    emailsApi.emailsTransactionalPost(emailData, callback);
-}); */
